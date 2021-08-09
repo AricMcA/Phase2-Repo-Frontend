26ec765055593d23334b00c6634038480978bb39
Aric McAlister <aricmca@gmail.com>
2021-08-09 14:58:34 -0500

    introduced state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import NavBar from "./components/NavBar"
 import TakeForm from "./components/TakeFrom";
-import TakeList from "./components/TakeList";
-
-
-const takes = [
-  { user: "user1", player:"player1", content:"content1" },
-  { user: "user2", player:"player2", content:"content2" },
-  { user: "user3", player:"player3", content:"content3" },
-  { user: "user4", player:"player4", content:"content4" },
-  { user: "user5", player:"player5", content:"content5" }
-]
+import TakeList from "./components/TakeList" 
 
 
 function App() {
+
+  const [ takes, setTakes ] = useState([
+    { user: "user1", player:"player1", content:"content1" },
+    { user: "user2", player:"player2", content:"content2" },
+    { user: "user3", player:"player3", content:"content3" },
+    { user: "user4", player:"player4", content:"content4" },
+    { user: "user5", player:"player5", content:"content5" }
+  ])
+
+  useEffect(()=> {
+    console.log('use effected')
+  }, [])
+
+  const addTake = take => {
+    const newTakes = [ ...takes, take ];
+    setTakes(newTakes);
+  }
+
   return (
     <div className="App">
       <NavBar />
 
       <h1>Welcome to NFL Debater</h1>
 
-      <TakeForm />
+      <TakeForm addTake={ addTake } />
 
       <TakeList takes={ takes } />
 
--- src/components/TakeFrom.js
+++ src/components/TakeFrom.js
@@ -1,20 +1,49 @@
+import React, { useState } from "react";
 
-const TakeForm = (props) => {
+
+
+const TakeForm = ({ addTake }) => {
+
+    const [ take, setTake ] = useState({
+        user: '',
+        player: '',
+        content: ''
+    })
+
+    const handleChange = event => {
+        setTake({
+            ...take,
+            [event.target.name]: event.target.value
+        })
+    }
+
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        
+        addTake(take);
+        setTake({
+            user:'',
+            player:'',
+            content:''
+        })
+    }
+
     return (
         <>
         <h3>Create a HotTake</h3>
-        <form>
+        <form onSubmit={ handleSubmit }>
             <div>
                 <label>User</label>
-                <input type="text" />
+                <input type="text" name="user" value={ take.user } onChange={ handleChange } />
             </div>
             <div>
                 <label>Player</label>
-                <input type="text" />
+                <input type="text" name="player" value={ take.player } onChange={ handleChange } />
             </div>
             <div>
                 <label>Content</label><br/>
-                <textarea />
+                <textarea name="content" value={ take.content } onChange={ handleChange } />
             </div>
             
             <input type="submit" value="Release HotTake" />
